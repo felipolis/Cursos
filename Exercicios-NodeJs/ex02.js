b6a605c26bab14aec03e152f1eff49d971977311
Felipe Archanjo <felipecunhamendes@gmail.com>
2021-09-23 22:33:51 -0300

    :package:

--- Exercicios-NodeJs/ex02.js
+++
@@ -1,8 +0,0 @@
-//Maximo entre dois valores
-
-function max(num1, num2){
-    return (num1 > num2 ? num1 : num2);
-}
-
-let valorMaior = max(10,10);
-console.log(valorMaior);
--- Exercicios-NodeJs/projeto.js
+++
@@ -1,9 +0,0 @@
-function VerificarEntrada(){
-    NomeConvidado = document.getElementById('nome').value;
-    ConvidadosFelipe = ['Felipe', 'Pamella', 'Rafael'];
-    if(ConvidadosFelipe.includes(NomeConvidado)){
-        document.getElementById('PermissaoDeEntrada').innerText = 'Você pode entrar!'
-    } else{
-        document.getElementById('PermissaoDeEntrada').innerText = 'Você não pode entrar!'
-    }
-}
