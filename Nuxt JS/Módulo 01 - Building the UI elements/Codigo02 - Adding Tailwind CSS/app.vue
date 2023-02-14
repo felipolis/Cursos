e9150dc1808e876f263bc606ffb447efa6fe1d56
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-02-14 01:57:40 -0300

    Curso Nuxt 3

---
+++ Nuxt JS/Módulo 01 - Building the UI elements/Codigo02 - Adding Tailwind CSS/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <!-- Testando o tailwindcss -->
+    <div class="bg-blue-500 text-white p-4">
+      <h1 class="text-2xl">Hello World</h1>
+    </div>
+  </div>
+</template>
