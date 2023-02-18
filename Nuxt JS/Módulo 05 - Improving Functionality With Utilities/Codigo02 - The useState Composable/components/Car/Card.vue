a2c43a3327982909c84d0045a5d0a509dd96eb29
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-02-18 18:28:42 -0300

    Add Modulos 5 e 6

---
+++ Nuxt JS/Módulo 05 - Improving Functionality With Utilities/Codigo02 - The useState Composable/components/Car/Card.vue
@@ -0,0 +1,37 @@
+
+<script setup>
+import heartFilled from '@/assets/heartFilled.png'
+import heartOutline from '@/assets/heartOutline.png'
+    const props = defineProps({
+        car: Object
+    })
+
+    const favored = useState(`favored-${props.car.id}`, () => {
+        return false
+    })
+</script>
+
+<template>
+    <div 
+        class="relative shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+    >   
+        <img 
+            class="absolute w-7 right-5 top-2 z-20" 
+            :src="favored ? heartFilled : heartOutline"
+            alt="" 
+            @click="favored = !favored">
+        <div class="flex h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
+            <NuxtImg 
+            :src="car.url" alt=""
+            class="w-[300px] h-full"
+            />
+            <div class="p-4 flex flex-col">
+                <div>
+                    <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
+                    <p>{{ car.description }}</p>
+                </div>
+                <h1 class="mt-auto text-xl">${{ car.price }}</h1>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ Nuxt JS/Módulo 06 - Revisiting the Skipped Functionality/Codigo01 - Adding the Car Make Filter/components/Car/SideBar.vue
@@ -0,0 +1,83 @@
+
+<script setup>
+    const { makes } = useCars()
+
+    const modal = ref({
+        make: false,
+        location: false,
+        price: false
+    })
+
+    const city = ref('')
+    const route = useRoute()
+
+    const updateModal = (key) => {
+        modal.value[key] = !modal.value[key]
+    }
+
+    const onChangeLocation = () => {
+        if (!city.value) return
+        if(!isNaN(parseInt(city.value))) {
+            throw createError({
+                statusCode: 400,
+                message: `Inavlid city format`
+            })
+        }
+
+        updateModal('location')
+        navigateTo(`/city/${city.value}/car/${route.params.make}`)
+        city.value = ''
+    }
+
+    const onChangeMake = (make) => {
+        updateModal('make')
+        navigateTo(`/city/${route.params.city}/car/${make}`)
+    }
+</script>
+
+<template>
+    <div class="shadow border w-64 mr-10 x-30 h-[190px]">
+        <!-- LOCATION START -->
+        <div class="p-5 flex justify-between relative cursor-pointer border-b">
+            <h3>Location</h3>
+            <h3 
+                @click="updateModal('location')" 
+                class="text-blue-400 capitalize"
+            >
+                {{ route.params.city }}
+            </h3>
+            <div 
+                v-if="modal.location" 
+                class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white"
+            >
+                <input type="text" class="border p-1 rounded" v-model="city">
+                <button @click="onChangeLocation" class="bg-blue-400 w-full mt-2 rounded text-white p-1">
+                Apply
+                </button>
+            </div>
+        </div>
+        <!-- LOCATION END -->
+
+        <!-- MAKE START -->
+        <div class="p-5 flex justify-between relative cursor-pointer border-b">
+            <h3>Make</h3>
+            <h3 class="text-blue-400 capitalize" @click="updateModal('make')">
+                {{ route.params.make || 'Any' }}
+            </h3>
+            <div 
+                class="absolute border shadow left-56 p-5 top-1 -m-1 w-[600px] flex justify-between flex-wrap bg-white z-50"
+                v-if="modal.make"
+            >
+                <h4 v-for="make in makes" :key="make" class="w-1/3" @click="onChangeMake(make)">
+                    {{ make }}
+                </h4>
+            </div>
+        </div>
+        <!-- MAKE END -->
+
+        <div class="p-5 flex justify-between relative cursor-pointer">
+            <h3>Price</h3>
+            <h3 class="text-blue-400 capitalize">Any</h3>
+        </div>
+    </div>
+</template>
